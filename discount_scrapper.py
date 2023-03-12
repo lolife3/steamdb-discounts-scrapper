564ac3e13519eafbc293a389006433aa941e2a82
bambuss <114941604+lolife3@users.noreply.github.com>
2023-03-12 19:59:56 +0100

    Update discount_scrapper.py

--- discount_scrapper.py
+++ discount_scrapper.py
@@ -3,7 +3,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.options import Options
 import pandas as pd
 import undetected_chromedriver as uc
 import timeit
